8c63abe66e64055050f02321ddba149bf01350e9
hasheddan <georgedanielmangum@gmail.com>
2021-03-08 18:05:04 -0600

    Add organization membership controller
    Adds an organization membership controller. This controller takes a
    naive approach to error handling and does not have unit tests, but is
    being merged early to allow other folks to continue to make progress on
    this provider.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ pkg/clients/client.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package clients
+
+import (
+	"context"
+
+	"github.com/google/go-github/v33/github"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+
+	"github.com/crossplane-contrib/provider-github/apis/v1beta1"
+)
+
+// GetConfig gets the config.
+func GetConfig(ctx context.Context, c client.Client, mg resource.Managed) ([]byte, error) {
+	pc := &v1beta1.ProviderConfig{}
+	if err := c.Get(ctx, types.NamespacedName{Name: mg.GetProviderConfigReference().Name}, pc); err != nil {
+		return nil, errors.Wrap(err, "cannot get referenced ProviderConfig")
+	}
+
+	t := resource.NewProviderConfigUsageTracker(c, &v1beta1.ProviderConfigUsage{})
+	if err := t.Track(ctx, mg); err != nil {
+		return nil, errors.Wrap(err, "cannot track ProviderConfig usage")
+	}
+
+	return resource.CommonCredentialExtractor(ctx, pc.Spec.Credentials.Source, c, pc.Spec.Credentials.CommonCredentialSelectors)
+}
+
+// NewClient creates a new client.
+func NewClient(token string) *github.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc)
+}
